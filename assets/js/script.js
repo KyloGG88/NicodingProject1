8558287797e179048f62707e914334e43dad33a8
shemlock <147415364+shemlock@users.noreply.github.com>
2024-01-08 14:32:40 +0000

    Update script.js
    Updated API Key for Youtube

--- assets/js/script.js
+++ assets/js/script.js
@@ -253,7 +253,7 @@ function showMovies(data) {
 }
 
 function get_video(searchTerm, id) {
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchTerm}&key=AIzaSyAe4NoJ71qftk0Igwf8x-7vfKbXZQaASJo`;
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchTerm}&key=AIzaSyAUhuGAcWY1XrJ4kj07-rvnzW8W8UlR7wk`;
 
     fetch(url)
         .then(response => response.json())
